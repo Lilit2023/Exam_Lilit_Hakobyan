a78c9807f568740f8337b9995fc09f5138272115
Lilit <lilmankan@gmail.com>
2023-12-14 12:22:17 +0400

    GamePage

---
+++ testData_/testData.py
@@ -0,0 +1,10 @@
+class User():
+    def __init__(self, userName, password):
+        self.username = userName
+        self.password = password
+
+
+
+
+#URLs
+gamePageUrl = "https://orteil.dashnet.org/cookieclicker/"
